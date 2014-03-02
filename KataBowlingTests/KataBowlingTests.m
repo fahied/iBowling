4bd7fae1288477e978999cf1f5b50705816f4ae2
Muhammad Fahied <fahied@gmail.com>
2014-03-03 00:53:01 +0100

    unit tests
    1. testNoBonus
    2. testPerfect
    3. testAllSpare

--- KataBowlingTests/KataBowlingTests.m
+++ KataBowlingTests/KataBowlingTests.m
@@ -7,9 +7,12 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "Game.h"
 
 @interface KataBowlingTests : XCTestCase
 
+
+@property (strong, nonatomic) Game *game;
 @end
 
 @implementation KataBowlingTests
@@ -17,18 +20,32 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.game = [[Game alloc] init];
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.game = nil;
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testNoBonus
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    NSInteger score = [self.game scoreFromString:@"9-9-9-9-9-9-9-9-9-9-"];
+    XCTAssertEqual(score, (NSInteger)90, @"should be 90");
 }
+
+- (void)testPerfect
+{
+    NSInteger score = [self.game scoreFromString:@"XXXXXXXXXXXX"];
+    XCTAssertEqual(score, (NSInteger)300, @"should be 300");
+}
+
+- (void)testAllSpare
+{
+    NSInteger score = [self.game scoreFromString:@"5/5/5/5/5/5/5/5/5/5/5"];
+    XCTAssertEqual(score, (NSInteger)150, @"should be 150");
+}
+
 
 @end
